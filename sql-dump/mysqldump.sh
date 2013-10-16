26eb3f9bd54df3394e557c2c5bca274949252d81
Diaconescu Radu <DiaconescuRadu@gmail.com>
2013-10-16 17:58:41 +0200

    New database scripts.

--- sql-dump/mysqldump.sh
+++ sql-dump/mysqldump.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#dumping the database for a certain database
 date=`date +%y%m%d`
-mysqldump --add-drop-table -h localhost\
- -u radi -p radi | bzip2\
- -c > $date$1.sql.bz2
+if [ $# -ne 1 ]; then
+	echo "Wrong number of params, please add the dump file";
+	exit
+fi
+
+if [ "$1" = "localhost" ]; then 
+    mysqldump --add-drop-table -h localhost\
+     -u radi -p radi | bzip2\
+     -c > $date$1.sql.bz2
+else
+    mysqldump --add-drop-table -h localhost\
+     -u radi -p radi | sed -e "s/localhost:10080/$1/g" | bzip2\
+     -c > $date$1.sql.bz2
+fi
