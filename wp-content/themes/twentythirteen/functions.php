421621bd8fa7598a5eeaa00cf76a5faca1866eab
Diaconescu Radu <DiaconescuRadu@gmail.com>
2013-11-06 01:29:25 +0100

    Cleaned up project.

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -749,7 +749,7 @@ function translate_title($title){
     return $title;
 }
 
-// Print the language switcher
+// Print the social icons in the navbar
 function get_social_icons(){
     ?>
     <div class="social_header">
@@ -768,6 +768,44 @@ function get_social_icons(){
     } else {
         echo '<a class="language_switcher" href="' . $translation_list[ro][url] . '">RO</a>';
     }?>
+    <?php
+}
+
+// Print the social icons
+function get_social_buttons(){
+    ?>
+    <div class="social_buttons"> <!-- Social buttons -->
+        <!-- facebook like button -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+        <!-- Facebook -->
+        <div class="fb-like" data-href="<?php the_permalink()?>" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+        <!-- Google plus button -->
+
+        <!-- Place this tag where you want the +1 button to render. -->
+        <div class="g-plusone" data-size="medium"></div>
+
+        <!-- Place this tag after the last +1 button tag. -->
+        <script type="text/javascript">
+          (function() {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = 'https://apis.google.com/js/plusone.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+        
+        <!-- Twitter -->
+
+        <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    </div>
     <?php
 }
 
--- wp-content/themes/twentythirteen/single.php
+++ wp-content/themes/twentythirteen/single.php
@@ -25,38 +25,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
-                <div class="social_buttons"> <!-- Social buttons -->
-                    <!-- facebook like button -->
-                    <div id="fb-root"></div>
-                    <script>(function(d, s, id) {
-                      var js, fjs = d.getElementsByTagName(s)[0];
-                      if (d.getElementById(id)) return;
-                      js = d.createElement(s); js.id = id;
-                      js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1";
-                      fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));</script>
-
-                    <!-- Facebook -->
-                    <div class="fb-like" data-href="<?php the_permalink()?>" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
-                    <!-- Google plus button -->
-
-                    <!-- Place this tag where you want the +1 button to render. -->
-                    <div class="g-plusone" data-size="medium"></div>
-
-                    <!-- Place this tag after the last +1 button tag. -->
-                    <script type="text/javascript">
-                      (function() {
-                        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-                        po.src = 'https://apis.google.com/js/plusone.js';
-                        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-                      })();
-                    </script>
-                    
-                    <!-- Twitter -->
-
-                    <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                </div>
+                <?php get_social_buttons() ?>
                 <div class="horizontalRule site_content"></div>
                 <?php twentythirteen_post_nav(); ?>
                 <div class="horizontalRule site_content"></div>
