aef6c8053dee2f35b168f67942abbbf8ec585f7f
Diaconescu Radu <radi@debian>
2013-11-25 23:58:48 +0100

    Small updates.

--- wp-content/themes/twentythirteen/front-page.php
+++ wp-content/themes/twentythirteen/front-page.php
@@ -53,62 +53,122 @@ get_header(); ?>
             $this->alt = $alt;
        }
     }
-    
-    $sliders = array (
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_1.jpg',
-                'http://diaconescuradu.com/2013/04/bivuac-pe-piatra-iernii-si-din-nou.html',
-                'Bivuac pe piatra iernii',
-                'Bivuac pe piatra iernii'),
+    if(ICL_LANGUAGE_CODE == 'en'){
+        $sliders = array (
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_1.jpg',
+                    'http://diaconescuradu.com/2013/04/bivuac-pe-piatra-iernii-si-din-nou.html',
+                    'Bivouac on Winterstein',
+                    'Bivuac pe piatra iernii'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_2.jpg',
-                'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-6-via-classica-din.html',
-                'Via Classica din Wilder Kaiser',
-                'Via Classica din Wilder Kaiser'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_2.jpg',
+                    'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-6-via-classica-din.html',
+                    'Via Classica from Wilder Kaiser',
+                    'Via Classica din Wilder Kaiser'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_3.jpg',
-                'http://diaconescuradu.com/2012/01/inceput-de-in-fagaras.html',
-                'Vanatoarea lui Buteanu',
-                'Vanatoarea lui Buteanu'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_3.jpg',
+                    'http://diaconescuradu.com/2012/01/inceput-de-in-fagaras.html',
+                    'Vanatoarea lui Buteanu',
+                    'Vanatoarea lui Buteanu'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_4.jpg',
-                'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-4-soclul-din.html',
-                'Wilder Kaiser',
-                'Wilder Kaiser'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_4.jpg',
+                    'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-4-soclul-din.html',
+                    'Wilder Kaiser',
+                    'Wilder Kaiser'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_5.jpg',
-                'http://diaconescuradu.com/2013/06/de-la-bordeluri-pe-apa-la-mori-de-vant.html',
-                'Olanda pe bicicleta',
-                'Olanda pe bicicleta'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_5.jpg',
+                    'http://diaconescuradu.com/2013/06/de-la-bordeluri-pe-apa-la-mori-de-vant.html',
+                    'Olanda on a bike',
+                    'Olanda pe bicicleta'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_6.jpg',
-                'http://diaconescuradu.com/explore/mode/reu/search/categories/drumetii',
-                'Drumetii in dolomiti',
-                'Drumetii in dolomiti'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_6.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/drumetii',
+                    'Trekking in the Dolomites',
+                    'Drumetii in dolomiti'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_7.jpg',
-                'http://diaconescuradu.com/category/dolomiti',
-                'Dolomiti, imparatia de stanca',
-                'Dolomiti, imparatia de stanca'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_7.jpg',
+                    'http://diaconescuradu.com/category/dolomiti',
+                    'The Dolomites, a kingdom of stone',
+                    'Dolomiti, imparatia de stanca'),
 
-                new Slider(content_url() . '/uploads/header_slider_images/cover_photo_8.jpg',
-                'http://diaconescuradu.com/2011/11/lepsezi.html',
-                'Lespezi',
-                'Lespezi'),
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_8.jpg',
+                    'http://diaconescuradu.com/2011/11/lepsezi.html',
+                    'Lespezi',
+                    'Lespezi'),
 
-                 new Slider(content_url() . '/uploads/header_slider_images/cover_photo_9.jpg',
-                'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului/germania',
-                'De pe dealurilei Germaniei',
-                'De pe dealurilei Germaniei'),
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_9.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului/germania',
+                    'On the hills of Germany',
+                    'De pe dealurilei Germaniei'),
 
-                 new Slider(content_url() . '/uploads/header_slider_images/cover_photo_10.jpg',
-                'http://diaconescuradu.com/explore/mode/reu/search/categories/fagaras',
-                'Pe creste Fagarasului',
-                'Pe creste Fagarasului'),
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_10.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/fagaras',
+                    'The Fagaras Mountains',
+                    'Pe creste Fagarasului'),
 
-                 new Slider(content_url() . '/uploads/header_slider_images/cover_photo_11.jpg',
-                'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului',
-                'Din al nostru Crai',
-                'Din al nostru Crai'));
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_11.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului',
+                    'From Piatra Craiului',
+                    'Din al nostru Crai'));
+    }
+    if(ICL_LANGUAGE_CODE == 'ro'){
+        $sliders = array (
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_1.jpg',
+                    'http://diaconescuradu.com/2013/04/bivuac-pe-piatra-iernii-si-din-nou.html',
+                    'Bivuac pe piatra iernii',
+                    'Bivuac pe piatra iernii'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_2.jpg',
+                    'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-6-via-classica-din.html',
+                    'Via Classica din Wilder Kaiser',
+                    'Via Classica din Wilder Kaiser'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_3.jpg',
+                    'http://diaconescuradu.com/2012/01/inceput-de-in-fagaras.html',
+                    'Vanatoarea lui Buteanu',
+                    'Vanatoarea lui Buteanu'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_4.jpg',
+                    'http://diaconescuradu.com/2013/08/wilder-kaiser-ziua-4-soclul-din.html',
+                    'Wilder Kaiser',
+                    'Wilder Kaiser'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_5.jpg',
+                    'http://diaconescuradu.com/2013/06/de-la-bordeluri-pe-apa-la-mori-de-vant.html',
+                    'Olanda pe bicicleta',
+                    'Olanda pe bicicleta'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_6.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/drumetii',
+                    'Drumetii in dolomiti',
+                    'Drumetii in dolomiti'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_7.jpg',
+                    'http://diaconescuradu.com/category/dolomiti',
+                    'Dolomiti, imparatia de stanca',
+                    'Dolomiti, imparatia de stanca'),
+
+                    new Slider(content_url() . '/uploads/header_slider_images/cover_photo_8.jpg',
+                    'http://diaconescuradu.com/2011/11/lepsezi.html',
+                    'Lespezi',
+                    'Lespezi'),
+
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_9.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului/germania',
+                    'De pe dealurilei Germaniei',
+                    'De pe dealurilei Germaniei'),
+
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_10.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/fagaras',
+                    'Pe creste Fagarasului',
+                    'Pe creste Fagarasului'),
+
+                     new Slider(content_url() . '/uploads/header_slider_images/cover_photo_11.jpg',
+                    'http://diaconescuradu.com/explore/mode/reu/search/categories/piatra-craiului',
+                    'Din al nostru Crai',
+                    'Din al nostru Crai'));
+
+
+    }
 
 
     if(ICL_LANGUAGE_CODE == 'en'){
---
+++ wp-content/themes/twentythirteen/photo-gallery.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Full Content Template
+ *
+   Template Name:  Photo gallery
+ *
+ * @file           full-width-page.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2011 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/full-width-page.php
+ * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
+ * @since          available since Release 1.0
+ */
+
+get_header(); ?>
+
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+        $(window).load(function(){ $('#gallery1').masonry({
+             columnWidth: 10,
+             itemSelector: '.gallery_image',
+             isFitWidth: true
+        }); });
+    });
+</script>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<div class="entry-content gallery-width photo-page">
+                        <header class="entry-header">
+                            <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+                            <div class="entry-thumbnail">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <?php endif; ?>
+
+                            <h1 class="entry-title"><?php the_title(); ?></h1>
+                        </header><!-- .entry-header -->
+
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+					</div><!-- .entry-content -->
+
+                    <?php get_social_buttons() ?>
+
+					<footer class="entry-meta">
+						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+					</footer><!-- .entry-meta -->
+				</article><!-- #post -->
+
+				<?php comments_template(); ?>
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
