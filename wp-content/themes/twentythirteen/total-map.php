5b3d0970691854906b17e63c1e77a4ff7eb1faf4
Diaconescu Radu <radi@debian>
2014-03-06 21:38:21 +0100

    Added map changes.

---
+++ wp-content/themes/twentythirteen/total-map.php
@@ -0,0 +1,33 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Full Content Template
+ *
+   Template Name:  TotalMap
+ *
+ * @file           full-width-page.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2011 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/full-width-page.php
+ * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
+ * @since          available since Release 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+        <div style="text-align: center;">
+          <iframe src="http://traxcatalog.net/radu" height="800" width="1250">
+          </iframe>
+        </div>
+</div><!-- end of #content content -->
+</div><!-- end of #content primary -->
+
+<?php get_footer(); ?>
