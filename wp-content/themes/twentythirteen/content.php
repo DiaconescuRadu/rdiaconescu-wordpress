ca31edf77f853600cb3ff442ae1d5e70d6c85587
Diaconescu Radu <radi@debian>
2018-01-03 21:49:39 +0100

    Added end of post line.

--- wp-content/themes/twentythirteen/content.php
+++ wp-content/themes/twentythirteen/content.php
@@ -47,7 +47,7 @@
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 	<?php else : ?>
-	<div class="entry-content">
+	<div class="entry-content end_div">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 	</div><!-- .entry-content -->
